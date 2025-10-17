14603eb5d5b0731abb2f8739094d293734722fe5
DenerMilGrau <leitenegridener@gmail.com>
2025-10-17 13:01:52 -0300

    Adicionar decorator às rotas (Dener)

--- main.py
+++ main.py
@@ -935,4 +935,4 @@ def deletar_lanche_insumo():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5002)
